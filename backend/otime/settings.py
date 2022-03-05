c049a1c3a47b6d15a4559ac41fab73cfb0f3aac4
Rafael Horacio <rafael.horacio@escolar.ifrn.edu.br>
2022-03-05 16:07:00 -0300

    Configurando o aplicativo

--- backend/otime/settings.py
+++ backend/otime/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7sc11=m!ue(h0pbm^c4e^wupl-s)(zxizrmbd08r0i*4k)wm_*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['otime-api2.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
